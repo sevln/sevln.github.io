742f7b0c43c5ccd96216ec35737a6460bff1a635
Evelyn S <elxsham@hotmail.com>
2015-05-09 17:50:52 -0400

    Added BootStrap Carousel, external js files
    - index.html - v2.0.5
    - added email.js, start-fullpage.js (moved from index.html to external
    js scripts)
    - custom.css - v0.3

---
+++ js/email.js
@@ -0,0 +1,3 @@
+/*Protected email script by Joe Maller. JavaScripts available at http://www.joemaller.com.*/
+					emailE=('evelyn.sham@' + 'mail.utoronto.ca')
+					document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
---
+++ js/start-fullpage.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['Top', 'About', 'Projects'],
+		sectionsColor: ['#fafafa', 'fafafa', '#343434'],
+		css3: true
+	});
+});
